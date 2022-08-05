dcdc2c02e3ffa3103c109abb4cb036ebb214144c
Olaf Sanburg <osanburg@gmail.com>
2022-08-04 18:12:42 -0700

    RPMX-19 User and Manager Class

--- User.java
+++ User.java
@@ -1,34 +1,73 @@
+package starprogrammers;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Date;
+import java.util.Scanner;
 /** 
     A user is able to access and the necessary functions of the hotel such as search for the type of room they desire,
      book a reservation, and view, change or cancel their reservation
 */
-class User {
+public class User {
     /**
         user selects a room from the list of available rooms displayed by program, and a new reservation is made under the user's name 
      */
-    bookRoom(){}
+    void bookRoom(){
+        String name, emailAddr, payment;
+        int age, partyNum;
+        Date checkIn, checkOut;
+        Scanner input = new Scanner(System.in);
+        // user inputs check in and check out dates then selects type of room
+
+        //TODO: Create GUI to allow user to input dates and select a type of room
+
+        // program displays whether room is available, if so prompt user for info(name, age, email address, # of people, payment, check in & out dates)
+        System.out.print("Enter name: ");
+        name = input.next();
+        System.out.print("Enter age: ");
+        age = input.nextInt();
+        System.out.print("Enter number of people in party: ");
+        partyNum = input.nextInt();
+        System.out.print("Enter name: ");
+        // if all prompts successfully completed, create new reservation and email confirmation to user
+    }
 
     /**
         user selects a type of room offered, then program displays a list of rooms that match the catagory
      */
-    searchForRoomType(){}
+    void searchForRoomType(){}
 
     /**
         user inputs identifying data(name?, password?, payment info?), if successful then their reservation is deleted from the database
      */
-    cancelReservation(){}
+    void cancelReservation(){}
 
     /**
         user inputs identifying data(name?, password?, payment info?), if successful then they are given the option to request a different type of room
      */
-    changeReservation(){}
+    void changeReservation(){}
 
     /**
         user inputs identifying data(name?, password?, payment info?), if successful then information regarding their reservation is displayed
      */
-    viewReservation(){}
+    void viewReservation(){}
 }
 
-class manager extends user{
-    
+class manager extends User{
+
+    /**
+        user selects reservation from database then selects an unbooked room to replace current room
+     */
+    void changeRoomReservation(){}
+
+    /**
+        user selects reservation from database, reservation is then deleted from database
+     */
+    void cancelRoomReservation(){}
+
+    /** 
+        user selects reservation (or booked room?) and change occupied(or checked-in flag?) to true
+    */
+    void checkCustomerIn(){}
 }
