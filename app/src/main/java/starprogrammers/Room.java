0f20432f9f94069f3c2b5832f4bdcaed7d23c2ef
hillard staggs <hillardwilliam.staggs.516@my.csun.edu>
2022-08-16 21:22:37 -0700

    update javadoc

--- app/src/main/java/starprogrammers/Room.java
+++ app/src/main/java/starprogrammers/Room.java
@@ -8,9 +8,10 @@
 //g)used string and int that are required to have in this code because that plans from UML diagram
 //after return with string to able show the more details for room
 
+package starprogrammers;
+
 import java.io.PrintWriter;
 import java.util.Scanner;
-
 //a public class represents show rooms at the hotel like checking the room and looking 
 //around to interesting in what hotel will offer for the room that customers can stay there at hotel
 /** 
@@ -19,100 +20,138 @@ import java.util.Scanner;
  * Type are specific for room and bed type to able fit how many people 
  * */
 
-public class Room implements Comparable<Room> { 
-    private String customerName; 
-    private int roomNumber;
-    private String roomType;
-    private String bedType;
-    private int bedQuantity;
-    private int roomPrice;
-    private int numberOfOccupants;
+public class Room implements Comparable<Room> {
+  private String firstName;
+  private String lastName;
+  private int roomNumber;
+  private String roomType;
+  private String bedType;
+  private int bedQuantity;
+  private int roomPrice;
+  private int numberOfOccupants;
+    
+/**
+ * To create a new instance of class room
+*/
 
-    /**
-     * To create a new instance of class room
-     */
-    public Room() { //a room class that represents to checking one of the room at a hotel
-
-    }
+  public Room() { //a room class that represents to checking one of the room at a hotel
+  }
     //each room has a variable to represent the room on which type and information from checking the room
-    public Room(String customerName,int roomNumber,String roomType,String bedType,int bedQuantity,int roomPrice,int numberOfOccupants) {
-        this.customerName = customerName;
-        this.roomNumber = roomNumber;
-        this.roomType = roomType;
-        this.bedType = bedType;
-        this.bedQuantity = bedQuantity;
-        this.roomPrice = roomPrice;
-        this.numberOfOccupants = numberOfOccupants;
-    }
-
-    /**
+  public Room(String lastName, String firstName, int roomNumber, String roomType, String bedType, int bedQuantity,
+      int roomPrice, int numberOfOccupants) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.roomNumber = roomNumber;
+    this.roomType = roomType;
+    this.bedType = bedType;
+    this.bedQuantity = bedQuantity;
+    this.roomPrice = roomPrice;
+    this.numberOfOccupants = numberOfOccupants;
+  }
+    
+  /**
      * to create the method if changing the selection of room type
      * to able to call the type of bed and room after this method if want to change
      */
-    public void changeRoomType(){
 
-    }
-
-    public int getnumberOfOccupants() { return numberOfOccupants; }
+  public int getnumberOfOccupants() {
+    return numberOfOccupants;
+  }
 
     /**
      * return the information for room 
      * @return the integer to used in this room for hotel
      */
-    public String customerName(){
-        return customerName;
-    }
 
-    public String roomType(){
-        return roomType;
-    }
+  public String customerName() {
+    return firstName + " " + lastName;
+  }
 
-    public String bedType(){
-        return bedType;
-    }
+  public String getRoomType() {
+    return roomType;
+  }
 
+  public String getBedType() {
+    return bedType;
+  }
+
+  public int getRoomNumber() {
+    return roomNumber;
+  }
+
+  public int getPrice() {
+    return roomPrice;
+  }
+  public String getFirstName(){
+   
+    return firstName;
+  }
+  public String getLastName(){
+    if(lastName == null){
+      return null;
+    }
+    return lastName;
+  }
+  public int getBedQuantity(){
+    return bedQuantity;
+  }
+   
     /**
      * To scan the input to able to get the more details after selecting the room
      */
-    public void readFrom(Scanner input) { //checking the next input as int
-        roomNumber = input.nextInt(); 
-        input.useDelimiter("/");
-        input.reset();
-        bedQuantity = input.nextInt(); //checking or scanning the quantity of bed
-        roomPrice = input.nextInt(); //checking or scanning the price to book a room at the hotel
-        numberOfOccupants = input.nextInt();//checking how many people can stay in that room
-    }
 
-    /**
+  public void readFrom(Scanner input) {
+    roomNumber = input.nextInt();
+    input.useDelimiter("/");
+    input.reset();
+    bedQuantity = input.nextInt();
+    roomPrice = input.nextInt();
+    numberOfOccupants = input.nextInt();
+  }
+
+      /**
      * to get scanning for output in each of output.print
      * @param output
      */
-    public void writeTo(PrintWriter output) { //print an information for room details
-        output.print(customerName); //each of output show what details for specific in room at hotel
-        output.print(" ");
-        output.print(roomNumber); //details of room number
-        output.print(" "); 
-        output.print(roomType); //details of type room like has suite in that room or something
-        output.print(" ");
-        output.print(bedType); //queen, king, sofebed, or full
-        output.print(" ");
-        output.print(bedQuantity); //how many of the quantities does the bed have in one room?
-        output.print(" ");
-        output.print(roomPrice); //a price to book a room at a hotel
-        output.print(" ");
-        output.println(numberOfOccupants); //number of occupants in per room
-    }
 
-    public String toString() { //scanned from the output
-        return customerName + "(" + numberOfOccupants + "):" +roomType + " " + bedType; //this is where if the user want to book for that specific room
-    }
+  public void writeTo(PrintWriter output) { //print an information for room details
+    output.print(firstName); //print first name 
+    output.print(" ");
+    output.print(lastName); //print last name
+    output.print(" ");
+    output.print(roomNumber); //details of room number
+    output.print(" ");
+    output.print(roomType);//details of type room like has suite in that room or something
+    output.print(" ");
+    output.print(bedType); //queen, king, sofebed, or full
+    output.print(" ");
+    output.print(bedQuantity);//how many of the quantities does the bed have in one room?
+    output.print(" ");
+    output.print(roomPrice);//a price to book a room at a hotel
+    output.print(" ");
+    output.println(numberOfOccupants); //number of occupants in per room
+  }
 
-    @Override
-    public int compareTo(Room t) {
-        if(roomPrice <= t.bedQuantity)
-            return -1;
-        else if(bedQuantity >= t.roomPrice)
-            return 1;
-        return 0;
-    }
+  public String toString() {
+    return "Room Number: " + roomNumber + " " + "Number of occupants allowed: " + numberOfOccupants + " Room Type: "
+        + roomType + " Bed Type: " + bedType; //this is where if the user want to book for that specific room
+  }
+
+  @Override
+  public int compareTo(Room t) {
+    if (roomPrice <= t.bedQuantity)
+      return -1;
+    else if (bedQuantity >= t.roomPrice)
+      return 1;
+    return 0;
+  }
+
+  public String roomInfo() { //pulling the details for room
+    return "\nPrice/Night: "
+        + roomPrice
+        + "\nType of room: "
+        + roomType
+        + "\nBed Quantity: " + bedQuantity
+        + "\nRoom Number: " + roomNumber;
+  }
 }
