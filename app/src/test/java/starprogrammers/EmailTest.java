0e7d78d1228c91833e37b67fb671118d95ba8a4a
MeowSalsa <100064587+MeowSalsa@users.noreply.github.com>
2022-08-18 19:18:24 -0700

    Working product here

--- app/src/test/java/starprogrammers/EmailTest.java
+++ app/src/test/java/starprogrammers/EmailTest.java
@@ -9,12 +9,12 @@ import org.junit.jupiter.api.Test;
 public class EmailTest {
     Reservation res = new Reservation("Smith", "John",
     65, "123546489", 
-    "johnsmith@gmail.com", 2, 206, 
+    "meowsauce123@gmail.com", 2, 206, 
     LocalDate.of(2022, 12, 1), 
     LocalDate.of(2022, 12, 3));
     Room room = new Room("Smith", "John", 206,
     "smoking", "King", 2, 50, 2);
-    Email email = new Email(res, room);
+    Email email = new Email(res, room, -1);
     
     @Test
     void testSendEmail() {
